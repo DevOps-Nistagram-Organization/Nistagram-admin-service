9f91f7186175de4faf8376f1ce406e657b52e366
DusanStevic <stevicdule@gmail.com>
2021-09-15 19:33:35 +0200

    feign admin service

--- src/main/java/com/nistagram/administrator/client/UserClient.java
+++ src/main/java/com/nistagram/administrator/client/UserClient.java
@@ -9,17 +9,18 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(url = "${api.userService}", name = "UserService")
+// app.user.url from application.properties
+@FeignClient(name = "user", url = "${app.user.url}")
 public interface UserClient {
-    @GetMapping(value = "getUser/{username}")
+    @GetMapping(value = "user/getUser/{username}")
     UserInfoDTO getUser(@PathVariable("username") String username);
 
-    @GetMapping(value = "getNotApprovedAgents")
+    @GetMapping(value = "user/getNotApprovedAgents")
     ResponseEntity<List<UserInfoDTO>> getNotApprovedAgents(@RequestHeader("Authorization") String bearerToken);
 
-    @PostMapping(value = "approveAgent")
+    @PostMapping(value = "user/approveAgent")
     ResponseEntity<UserInfoDTO> approveAgent(@RequestBody() UsernameWrapper dto, @RequestHeader("Authorization") String bearerToken);
 
-    @PostMapping(value = "rejectAgent")
+    @PostMapping(value = "user/rejectAgent")
     ResponseEntity<UserInfoDTO> rejectAgent(@RequestBody() UsernameWrapper dto, @RequestHeader("Authorization") String bearerToken);
 }
